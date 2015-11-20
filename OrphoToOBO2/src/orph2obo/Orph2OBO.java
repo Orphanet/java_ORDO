714b687b25a596de1d70dd1773be6b1ca6048ed6
celine14 <celine.rousselot@inserm.fr>
2015-11-20 15:53:52 +0100

    test maj github

--- OrphoToOBO2/src/orph2obo/Orph2OBO.java
+++ OrphoToOBO2/src/orph2obo/Orph2OBO.java
@@ -37,7 +37,7 @@ public class Orph2OBO {
     public static void main(String[] args) throws OWLOntologyCreationException, OWLOntologyStorageException {
 	
     /**This is the name of the directory where the Orphanet XML files will be written to*/
-    String directory = "OrphadataMayBACKUP";
+    String directory = "OrphadataMay";
 	
 	Orph2OBO o2o = new Orph2OBO();
 	o2o.createDownloadDirectoryIfDoesntExist(directory);
