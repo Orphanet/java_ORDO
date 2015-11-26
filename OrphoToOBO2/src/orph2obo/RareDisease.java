304ba350d4c85294ecac2db683602a2817f571aa
samuelDemarest <demarestsamuel@hotmail.com>
2015-11-26 12:04:42 +0100

    test final commit

--- OrphoToOBO2/src/orph2obo/RareDisease.java
+++ OrphoToOBO2/src/orph2obo/RareDisease.java
@@ -719,8 +719,6 @@ public void setInheritNum(String inheritNum) {
 			System.out.println("Genes: "+this.genelists.toString());
 			System.out.println("Smb: "+this.symbol.toString());
 			System.out.println("Type: "+this.geneTypeName.toString());
-			System.out.println("Type2: "+this.geneTypeName.toString());
-
 			for( int i =0; i<this.genelists.size(); i++){
 				//creating gene class and adding the symbol annotation and label annotation
 				OWLClass gene = owlvar.getFactory().getOWLClass(this.geneNum.get(i), owlvar.getPrefixmanager());
