24ef5b086ec06accef7e86aa7b1ffadf01523614
celine14 <celine.rousselot@inserm.fr>
2015-11-27 15:55:51 +0100

    Evolution pour faire fonctionner le parser Gene avec les nouvelles informations Locus et GenType

--- OrphoToOBO2/src/orph2obo/OrphaGenesXMLParser.java
+++ OrphoToOBO2/src/orph2obo/OrphaGenesXMLParser.java
@@ -15,7 +15,8 @@ import org.xml.sax.helpers.DefaultHandler;
 /**
  * Parse 	http://www.orphadata.org/data/xml/en_product6.xml 	
  * i.e., diseases and their associated genes.
- * @author peter
+ * @author peter 
+ * @author Celine Rousselot maj Nov2015
  *
  */
 public class OrphaGenesXMLParser extends DefaultHandler {
@@ -30,7 +31,9 @@ public class OrphaGenesXMLParser extends DefaultHandler {
 	private boolean within_geneAssociationType = false;
 	private ArrayList<Gene> genes;
 	private boolean within_geneAssociationStatus = false;
-	
+	private boolean in_locus_list = false;//UPDATE-CR261115-ajout des locus
+	private boolean in_gene_type = false;//UPDATE-CR261115 pour distinguer l'orphanumber du gene et du type
+
 	
 	
 	public OrphaGenesXMLParser () {
@@ -68,7 +71,8 @@ public class OrphaGenesXMLParser extends DefaultHandler {
 			Attributes attributes) throws SAXException {
 			//reset
 			tempVal = "";
-			//System.err.println("Gene : "+qName); 
+
+			//detection balise d'ouverture
 			if(qName.equalsIgnoreCase("Disorder")) {
 				currentDisease = null; // reset
 			} else if (qName.equalsIgnoreCase("DisorderGeneAssociationList")){
@@ -89,6 +93,11 @@ public class OrphaGenesXMLParser extends DefaultHandler {
 				within_geneAssociationStatus =true;
 			}else if (qName.equalsIgnoreCase("GeneList")){
 				in_gene_list=true;
+			}else if (qName.equalsIgnoreCase("LocusList")){ //UPDATE-CR261115-ajout des locus
+				in_locus_list=true;
+			}
+			else if (qName.equalsIgnoreCase("GeneType")){ //UPDATE-CR261115 pour distinguer l'orphanumber du gene et du type
+				in_gene_type=true;
 			}
 		}
 
@@ -99,29 +108,79 @@ public class OrphaGenesXMLParser extends DefaultHandler {
 
     public void endElement(String uri, String localName, String qName) 
 	throws SAXException {
-   if (qName.equalsIgnoreCase("Orphanumber") && in_gene_node){
-		//System.out.println(tempVal);
-		currentDisease.add_geneNum(tempVal);    		
-   }else if(qName.equalsIgnoreCase("Name") && within_geneAssociationType){ // Remettre Orphanmumber quand il seront présents dans les fichiers d'entrée
-		currentDisease.setGeneType(tempVal);
-		currentDisease.setgeneTypeName(tempVal);
-    }else if(qName.equalsIgnoreCase("Orphanumber")){
-		 if (!(in_gene_node) && !(within_geneAssociationType)){
+
+    	//=============================== GESTION DES ORPHANUMBER =============================================//
+   if(qName.equalsIgnoreCase("Orphanumber")){
+		 if (!(in_gene_node) && !(within_geneAssociationType)){ //infos sur Disorder - Orphanumber
+			 //System.out.println("disease orphanum" + tempVal);
 			 currentDisease = this.diseases.get(tempVal);
-		 }	//setting the gene-disease orphanumber
-    } else if (qName.equalsIgnoreCase("Symbol")) {
+		 }	
+		 else if (in_gene_node && !in_gene_type && !(within_geneAssociationType)){ //infos sur Gene - Orphanumber
+			//System.out.println("Gene Orphanum" + tempVal);
+				currentDisease.add_geneNum(tempVal); 
+		 }
+		 else if (in_gene_node && in_gene_type && !(within_geneAssociationType)){//infos sur GeneType - Orphanumber
+			//System.out.println("GeneType Orphanum" + tempVal);
+				//currentDisease.add_geneNum(tempVal); 
+		 }
+		// A FAIRE Orphanumber à rajouter dans fichier en_product6.xml
+		 else if (!in_gene_node && within_geneAssociationType){//infos sur DisorderGeneAssociationType - Orphanumber
+			//System.out.println("GeneAssociationType Orphanum" + tempVal);
+				currentDisease.setGeneType(tempVal); //A QUOI CA SERT?
+				//currentDisease.setGeneTypeOrphaNum(tempVal); //A FAIRE
+		 }
+   }
+   //======================================================================================================//
+   
+ //=============================== GESTION DES INFOS SUR GENE =============================================//
+   //infos sur gene - Symbol
+   else if (qName.equalsIgnoreCase("Symbol")) {
 		currentDisease.setSymbol(tempVal);
-	} else if (in_gene_node && qName.equalsIgnoreCase("Name") && !in_gene_list) { //ignorer la partie GeneList pour éviter les doublons de gènes
+	} 
+  
+
+ //infos sur gene - Name
+   else if (in_gene_node && qName.equalsIgnoreCase("Name") && !in_gene_type) { 
 		//System.out.println("you are in the gene name node and the name is :" + tempVal + " and the current disease object is :" + currentDisease);
 	    currentDisease.add_genes(tempVal);
-	   // System.out.println("the gene name has been added to the list");
-	} else if(within_geneAssociationType && qName.equalsIgnoreCase("Name")){
-		//System.out.println("Current disease object is :" + currentDisease + " and gene association type is : " + tempVal);
-		currentDisease.setgeneTypeName(tempVal);
-		
-	}else if (within_geneAssociationStatus && qName.equalsIgnoreCase("Name")){
+   } 
+   
+   //infos sur geneType - type name
+   else if (in_gene_node && qName.equalsIgnoreCase("Name") && in_gene_type) { 
+		System.out.println("you are in the gene name node and the name is :" + tempVal);
+		//currentDisease.setGenType(tempVal);CREATION setGenType
+   } 
+   
+   //infos sur les locus
+  	else if (qName.equalsIgnoreCase("GeneLocus")){
+  		System.out.println("Current disease object is :" + currentDisease + " and locus is : " + tempVal);
+  		//currentDisease.setGeneLocus(tempVal);//A FAIRE!
+  	}
+   
+   //infos sur les synonymes
+	else if (qName.equalsIgnoreCase("Synonym")){
+		System.out.println("Current disease object is :" + currentDisease + " and synonym is : " + tempVal);
+		currentDisease.setGeneSyn(tempVal);
+	}
+   
+   //======================================================================================================//
+   
+ //=============================== GESTION DES INFOS SUR ASSOCIATION GENE/DISORDER ========================//
+   //infos sur DisorderGeneAssociationType - status
+   else if (within_geneAssociationStatus && qName.equalsIgnoreCase("Name")){
 		currentDisease.setGeneTypeStatus(tempVal);
 	}
+   
+   //infos sur DisorderGeneAssociationType - name
+   else if(within_geneAssociationType && qName.equalsIgnoreCase("Name")){
+	   //System.out.println("you are in the gene name node and gentype and the name is :" + tempVal);
+		currentDisease.setgeneTypeName(tempVal);
+	}
+ //======================================================================================================//
+  
+ //=============================== GESTION DES INFOS SUR REFERENCES EXTERNES ============================//
+
+   //infos sur les external reference
 	else if (qName.equalsIgnoreCase("ExternalReference")){
 	    within_externalReferenceElement = false;
 	    tmpGene.addExternalReference(tmpExtRef);
@@ -133,21 +192,24 @@ public class OrphaGenesXMLParser extends DefaultHandler {
 		   qName.equalsIgnoreCase("Reference")){
 	  
 		currentDisease.setGeneRefs(tempVal);
-	} else if(qName.equalsIgnoreCase("Gene")) {
+	} 
+ //======================================================================================================//
+   
+   //detection balise de fermeture
+	else if(qName.equalsIgnoreCase("Gene")) {
 	    in_gene_node = false;
-	    
 	    this.genes.add(tmpGene);
 	}else if(qName.equalsIgnoreCase("GeneList")) {
 	    in_gene_list = false;
 	} else if (qName.equalsIgnoreCase("DisorderGeneAssociationType")){
 		within_geneAssociationType = false;
-	}else if (qName.equalsIgnoreCase("Synonym")){
-		//System.out.println("Current disease object is :" + currentDisease + " and synonym is : " + tempVal);
-		currentDisease.setGeneSyn(tempVal);
 	}else if (qName.equalsIgnoreCase("DisorderGeneAssociationStatus")){
 		within_geneAssociationStatus =false;
-		
+	}else if (qName.equalsIgnoreCase("GeneType")){
+		in_gene_type =false;
 	}
+   
+   
    /**else if (qName.equalsIgnoreCase("Disorder")) {
 	    RareDisease dxr = this.diseases.get(disease_orphanum);
 	    if (dxr == null) {
