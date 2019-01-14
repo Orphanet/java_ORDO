c028041acc9950aeca009788a44910c6debcb48b
samuelDemarest <demarestsamuel@hotmail.com>
2019-01-14 16:50:22 +0100

    Changelog utilisable sur eclipse (pour ORDO) et avec arguments

--- OrphoToOBO2/src/orph2obo/ChangeLog.java
+++ OrphoToOBO2/src/orph2obo/ChangeLog.java
@@ -13,19 +13,39 @@ public class ChangeLog {
     public static void main(String[] args) throws Ontology1LoadException, Ontology2LoadException {
     	FileOutputStream f;
 		PrintStream defaultOut =  System.out;
-		String product = "ORDO";
+		String prevFile      = "/OrphoToOBO2/ORDO_"+ORDOVariables.lang+"_"+ORDOVariables.prevVersion+".owl";
+		String newFile       = "/OrphoToOBO2/ORDO_"+ORDOVariables.lang+"_"+ORDOVariables.version+".owl";
+		String changeLogFile = "C:\\OrphoToOBO2\\bubastis_change_log_ORDO_"+ORDOVariables.lang+"_"+ORDOVariables.version+"_"+ORDOVariables.prevVersion;
+		/* prev version : "file:/OrphoToOBO2/ORDO_"+Conf.lang+"_"+Conf.prevVersion+".owl"
+		 * new version :  "file:/OrphoToOBO2/"+product+"_"+Conf.lang+"_"+Conf.version+".owl"
+		 * txt output "C:\\OrphoToOBO2\\bubastis_change_log_"+product+"_"+Conf.lang+"_"+Conf.version+"_"+Conf.prevVersion+".txt"
+		 * xml output "C:\\OrphoToOBO2\\bubastis_change_log_"+product+"_"+Conf.lang+"_"+Conf.version+"_"+Conf.prevVersion+".xml"
+		 * 
+		 */
+		
+		System.setOut(defaultOut);
+		
+		if(args.length>2){
+			System.out.println("using ARGS");
+			prevFile      = args[0];
+			newFile       = args[1];
+			changeLogFile = args[2];			
+		}else{
+			System.out.println("default files used (ORDO last generation)\nYou can specify input/output using args changelog.jar prevVersion.owl newVersion.owl changelogFile");
+		}
+		
 		//String version = "2.8";
-		System.out.println("Comparing versions "+Conf.version+" and "+Conf.prevVersion+" of "+product+" in "+Conf.lang);
+		System.out.println("Comparing versions "+prevFile+" and "+newFile+" and writing in "+changeLogFile+".xml/txt");
 		try {
-			 f = new FileOutputStream("C:\\OrphoToOBO2\\bubastis_change_log_"+product+"_"+Conf.lang+"_"+Conf.version+"_"+Conf.prevVersion+".txt");			 
+			 f = new FileOutputStream(changeLogFile+".txt");			 
 			 System.setOut(new PrintStream(f));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		CompareOntologies bubastis = new CompareOntologies();
-		bubastis.doFindAllChanges("file:/OrphoToOBO2/"+product+"_"+Conf.lang+"_"+Conf.prevVersion+".owl","file:/OrphoToOBO2/"+product+"_"+Conf.lang+"_"+Conf.version+".owl");
-		bubastis.writeDiffAsXMLFile("C:\\OrphoToOBO2\\bubastis_change_log_"+product+"_"+Conf.lang+"_"+Conf.version+"_"+Conf.prevVersion+".xml");
+		bubastis.doFindAllChanges("file:"+prevFile,"file:"+newFile);
+		bubastis.writeDiffAsXMLFile(changeLogFile+".xml");
 		System.setOut(defaultOut);
 		System.out.println("Exit program after saving Change Log");
     }
